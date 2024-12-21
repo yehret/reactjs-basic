6a2b223548ad97468f0fe1810fc767378b42c27f
Serhii Samoliuk <samolyk2005@gmail.com>
2024-12-21 15:21:54 +0100

    quiz

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import './index.scss';
 
 const questions = [
   {
-    title: 'React - is a/an ... ?',
+    title: 'What is React?',
     variants: ['library', 'framework', 'app'],
     correct: 0,
   },
@@ -20,39 +21,126 @@ const questions = [
     ],
     correct: 2,
   },
+  {
+    title: 'What is React Virtual DOM?',
+    variants: [
+      'Virtual representation of real DOM',
+      'Another name of React',
+      'Library to build websites',
+    ],
+    correct: 0,
+  },
+  {
+    title: 'What is React component life cycle?',
+    variants: [
+      'Stages of component existence',
+      'Method of react',
+      'Library to build websites',
+    ],
+    correct: 0,
+  },
+  {
+    title: 'What is React context?',
+    variants: [
+      'Library to build websites',
+      'It is an object that stores data',
+      'Way to pass data between components',
+    ],
+    correct: 2,
+  },
+  {
+    title: 'What is React hook?',
+    variants: [
+      'Function to interact with state of React',
+      'Library to build websites',
+      'It is an object that stores data',
+    ],
+    correct: 0,
+  },
+  {
+    title: 'What is React state?',
+    variants: [
+      'State is an object that stores data',
+      'State is a way to interact with DOM',
+      'State is a function',
+    ],
+    correct: 0,
+  },
+  {
+    title: 'What is React props?',
+    variants: [
+      'Props is a way to pass data between components',
+      'Props is a function',
+      'Props is an object that stores data',
+    ],
+    correct: 0,
+  },
+  {
+    title: 'What is React DOM?',
+    variants: [
+      'DOM is a virtual representation of real DOM',
+      'DOM is a way to interact with DOM',
+      'DOM is a function',
+    ],
+    correct: 0,
+  },
 ];
 
-function Result() {
+
+function Result({correct, startAgain}) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Ypu have anwered 3 out of 10</h2>
-      <button>Try again</button>
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="celebrate" />
+      <h2>You have anwered {correct} out of {questions.length}</h2>
+      <button onClick={startAgain}>Try again</button>
     </div>
   );
 }
 
-function Game() {
+function Game({ step, question, onClickVariant }) {
+  const progressPerc = Math.round((step / questions.length) * 100);
+
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${progressPerc}%` }} className="progress__inner"></div>
       </div>
-      <h1>What is useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Function for storing component's data</li>
-        <li>Global state</li>
-        <li>Thats when you are useless</li>
+        {question.variants.map((text, i) => (
+          <li onClick={() => onClickVariant(i)} key={i}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+
+    if (index === question.correct) setCorrect(correct + 1);
+  };
+
+  const startAgain = () => {
+    setStep(0);
+    setCorrect(0);
+  };
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game step={step} onClickVariant={onClickVariant} question={question} />
+      ) : (
+        <Result startAgain={startAgain} correct={correct}/>
+      )}
     </div>
   );
 }
