4596cc488591deb6f058cbdebd36fffdad7e1912
Serhii Samoliuk <samolyk2005@gmail.com>
2024-12-21 16:15:22 +0100

    user invite list

--- src/App.js
+++ src/App.js
@@ -6,10 +6,57 @@ import { Users } from './components/Users';
 // User list: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [inviteList, setInviteList] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (inviteList.includes(id)) {
+      setInviteList((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInviteList((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+   setSuccess(true)
+  }
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Error fetching users');
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={inviteList.length} />
+      ) : (
+        <Users
+          onClickInvite={onClickInvite}
+          inviteList={inviteList}
+          searchValue={searchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          isLoading={isLoading}
+          items={users}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,9 +4,11 @@ export const Success = ({ count }) => {
   return (
     <div class="success-block">
       <img src="/assets/success.svg" alt="Success" />
-      <h3>Успешно!</h3>
-      <p>Всем {count} пользователям отправлено приглашение.</p>
-      <button className="send-invite-btn">Назад</button>
+      <h3>Success!</h3>
+      <p>To all {count} users has been sent an invitation.</p>
+      <button onClick={() => window.location.reload()} className="send-invite-btn">
+        Return
+      </button>
     </div>
   );
 };
