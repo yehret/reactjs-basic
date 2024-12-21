d3714491e03085ceb7e674bd18456c4b2f437b78
Serhii Samoliuk <samolyk2005@gmail.com>
2024-12-21 14:21:45 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Counter:</h2>
+        <h1>0</h1>
+        <button className="minus">- Minus</button>
+        <button className="plus">Plus +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
