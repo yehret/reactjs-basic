8345aa0e7f6b8b8923257c793b53d41fd4ed4b9e
Serhii Samoliuk <samolyk2005@gmail.com>
2024-12-21 14:49:00 +0100

    modal by using components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import './index.scss';
 
-function App() {
-   const [open, setOpen] = React.useState(false)
-
-  return (
-    <div className="App">
-      <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Open modal</button>
-      {/* {open && <div className="overlay">
-        <div className="modal">
-          <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='img'/>
-        </div>
-      </div>} */}
+const Modal = ({open, setOpen}) => {
+   return (
       <div className={`overlay animated ${open ? 'show' : ''}`}>
         <div className="modal">
           <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
@@ -25,6 +12,16 @@ function App() {
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='img'/>
         </div>
       </div>
+   )
+}
+
+function App() {
+   const [open, setOpen] = React.useState(false)
+
+  return (
+    <div className="App">
+      <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Open modal</button>
+         <Modal open={open} setOpen={setOpen}/>
     </div>
   );
 }
